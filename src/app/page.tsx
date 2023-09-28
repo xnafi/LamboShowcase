f6f2daf32034ba9c3103674b4ca35299037578fa
xnafi <fk.nafi11@gmail.com>
2023-09-28 16:42:44 +0600

    navBar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HomePage from "./HomePage/page";
 
 export default function Home() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="flex flex-col justify-center items-center h-[100vh] w-[100vw]">
+      <HomePage />
+    </div>
   );
 }
