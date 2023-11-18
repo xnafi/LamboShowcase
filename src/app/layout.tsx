1cdc489c28e5a5657a904c8793dfce8222ae285a
xnafi <fk.nafi11@gmail.com>
2023-11-18 15:02:16 +0600

    issue fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
         <body
-          className={` ${inter.className} w-full max-w-[1400px] flex flex-col justify-center items-center mx-auto custom-cursor container`}
+          className={`w-full max-w-[1400px] flex flex-col justify-center items-center mx-auto custom-cursor container`}
           suppressHydrationWarning={true}
         >
           <Navbar />
