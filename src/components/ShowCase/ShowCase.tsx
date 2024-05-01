d7c3a3bad6715b93b652b6e345695f660c4264c3
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-05-01 17:10:54 +0600

    my showcase desgn change

--- src/components/ShowCase/ShowCase.tsx
+++ src/components/ShowCase/ShowCase.tsx
@@ -26,53 +26,17 @@ const ShowCase = () => {
   );
 };
 
-const ROTATION_RANGE = 32.5;
-const HALF_ROTATION_RANGE = 32.5 / 2;
-
 const TiltCard = () => {
-  const ref = useRef<HTMLDivElement | null>(null);
-
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  const xSpring = useSpring(x);
-  const ySpring = useSpring(y);
-
-  const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!ref.current) return [0, 0];
-
-    const rect = ref.current.getBoundingClientRect();
-
-    const width = rect.width;
-    const height = rect.height;
-
-    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
-    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
-
-    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
-    const rY = mouseX / width - HALF_ROTATION_RANGE;
-
-    x.set(rX);
-    y.set(rY);
-  };
-
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
-  };
-
   return (
     <div className="relative h-[60vh] md:h-[100vh] w-[90vw] md:max-w-[1400px] rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300">
-      {lamborghiniCars.slice(0, 10).map((car: allCarData) => (
+      {lamborghiniCars.slice(0, 1).map((car: allCarData) => (
         <Swiper
           key={car.id}
           pagination={true}
           modules={[Pagination]}
           className={`${styles.swiper} mySwiper`}
         >
-          <div className="absolute inset-4 rounded-xl bg-white shadow-lg">
+          <div className="absolute w-[98%] h-[96%] self-center mx-auto inset-0 rounded-xl bg-white shadow-lg">
             <Link
               title="click for details"
               className="cursor-pointer text-yellow-300  self-end sub-heading"
@@ -80,10 +44,10 @@ const TiltCard = () => {
             >
               <Image
                 src={car.image}
-                className="bgWrap pb-20 md:pb-10"
+                className={`pb-16 md:pb-8 !z-10 !h-[70%] self-center mx-auto`}
                 quality={100}
                 fill
-                sizes="90vw"
+                sizes="100vw"
                 alt=""
               />
             </Link>
