87819759d96d543b89d10f5d4b7bacc084dd7cad
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-05-16 18:05:45 +0600

    vercel error

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,6 @@ import { useScroll } from "framer-motion";
 import Banner from "../../components/Banner/Banner";
 import ShowCase from "../showcase/page";
 import { useRef } from "react";
-import CarDetails from "@/components/CarDetails/CarDetails";
 
 export default function Home() {
   const container = useRef<HTMLDivElement | null>(null);
