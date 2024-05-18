6f85d7fde983cfba54a9c04b7441a64243e674ed
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-05-18 16:22:37 +0600

    showcase and banner scroll animation done

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -1,16 +1,23 @@
 import ShowCaseItems from "@/components/ShowCase/ShowCaseItems";
 import ShowCaseLayout from "../layout";
-import { useSpring, useTransform } from "framer-motion";
-// import { useSpring, useTransform, motion } from 'framer-motion';
+import { useSpring, useTransform, motion } from "framer-motion";
 
 const ShowCase = ({ scrollYProgress }: any) => {
   const scale = useTransform(scrollYProgress, [0, 100], [1, 0]);
-  const rotate = useTransform(scrollYProgress, [0, -100], [1, 100]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [2, -0]);
+  const opacity = useTransform(scrollYProgress, [-1, 1], [1, 1]);
+
   const springScale = useSpring(scale, { stiffness: 500, damping: 50 });
   const springRotate = useSpring(rotate, { stiffness: 500, damping: 50 });
+  const springOpacity = useSpring(opacity, { stiffness: 600, damping: 50 });
+
   return (
-    <div
-      style={{ scale: springScale, rotate: springRotate }}
+    <motion.div
+      style={{
+        scale: springScale,
+        rotate: springRotate,
+        opacity: springOpacity,
+      }}
       className="relative grid w-full place-content-center bg-gradient-to-br text-slate-900 place-items-center"
     >
       <div className="flex gap-x-3 md:gap-x-4">
@@ -19,7 +26,7 @@ const ShowCase = ({ scrollYProgress }: any) => {
       </div>
 
       <ShowCaseItems />
-    </div>
+    </motion.div>
   );
 };
 
