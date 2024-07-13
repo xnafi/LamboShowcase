db306709c83a87362df10673438f8e96ac7a7880
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-07-13 17:10:21 +0600

    api hidden

--- src/components/OurLocation/OurLocation.tsx
+++ src/components/OurLocation/OurLocation.tsx
@@ -4,6 +4,7 @@ import { GoogleMapComp } from "../LandingPage/GoogleMapComp/GoogleMapComp";
 export default function OurLocation() {
   return (
     <div>
+      <h1 className="uppercase headings text-white text-center my-6">Search us</h1>
       <GoogleMapComp />
     </div>
   );
