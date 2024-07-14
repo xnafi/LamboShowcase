686714b73635dbc25d9848ba96d9278f1a3a82c8
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-07-14 18:50:31 +0600

    vercel error

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -7,7 +7,7 @@ import logo from "../../assets/logoWhite.png";
 function Footer() {
   return (
     <footer className="px-4 divide-y w-[100%] h-full">
-      <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+      <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0 text-white">
         <div className="lg:w-1/3">
           <Link href="/">
             <Image
